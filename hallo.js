3941383139a0211fc241e056fe27566eea5aebf2
Maja <majamarek.design@gmail.com>
2020-08-05 15:19:16 +0200

    task 4 done

---
+++ hallo.js
@@ -0,0 +1,29 @@
+/* Declare two variables "apples" and "oranges". Assign a value of 20 to apples. Assign a value of 30 to oranges. */
+let apple = 20;
+let orange = 30;
+/* Check whether apples and oranges are equal. */
+console.log(apple === orange);
+/* Check whether apples and oranges are not equal. */
+console.log(apple !== orange);
+/* Check whether apples is greater than oranges. */
+console.log(apple > orange);
+/* Check whether oranges is greater than apples. */
+console.log(orange > apple);
+/* Declare another variable "mangoes" and give it a value of 5.
+ */ let mangoes = 5;
+/*  Multiply mangoes and apples, and check whether this result is greater than mangoes added to oranges.
+ */ console.log(mangoes * apple);
+console.log(mangoes + orange);
+/* Check whether oranges is greater than apples. */
+console.log(mangoes + orange > mangoes * apple);
+/* Declare another variable "mangoes" and give it a value of 5. Multiply mangoes and apples, and check whether this result is greater than mangoes added to oranges. */
+console.log(apple - mangoes);
+/* Subtract mangoes from apples and check whether this result is less than oranges divided by mangoes. */
+console.log(apple - mangoes < orange / mangoes);
+/* Check whether mangoes, apples and oranges are equal. */
+console.log((mangoes === orange) === apple);
+/* Check whether the remainder of apples divided by mangoes and the remainder of oranges divided by mangoes are equal. */
+console.log(apple % mangoes === orange % mangoes);
+/* Check whether mangoes added to apples is greater than oranges minus mangoes. If it is not, find an operator which will give a result of true. */
+console.log(mangoes + apple > orange - mangoes);
+/* BONUS CHALLENGE: Try increasing script readability by adding question headings to your output before each result. */
