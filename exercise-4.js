722b6d3123e586cbd1b35ffc3917530cb43fcce9
frausafiye <s_gulenc@outlook.com>
2020-08-05 14:04:07 +0200

    answered the questions

---
+++ exercise-4.js
@@ -0,0 +1,27 @@
+//0
+let apples = 20;
+let oranges = 30;
+//1
+console.log("1." + (apples == oranges));
+//2
+console.log("2." + (apples != oranges));
+//3
+console.log("3." + (apples > oranges));
+//4
+console.log("4." + (apples < oranges));
+//5
+console.log("5." + (oranges > apples));
+//6
+let mangoes = 5;
+console.log("6." + (mangoes * apples > mangoes + oranges));
+//7
+console.log("7." + (apples - mangoes < oranges / mangoes));
+//8
+console.log("8." + ((apples == oranges) == mangoes));
+//9
+console.log("9." + (apples % mangoes == oranges % mangoes));
+//10
+console.log("10.a." + (mangoes + apples > oranges - mangoes));
+
+console.log("10.b." + (mangoes + apples >= oranges - mangoes));
+//11 : added question headings.
