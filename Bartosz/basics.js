86b1fbf114ce73123e641520704dd9fedc157881
bartxs <b.soltysik90@gmail.com>
2020-08-05 14:08:40 +0200

    Exercise 4

---
+++ Bartosz/basics.js
@@ -0,0 +1,37 @@
+// 0.
+let apples = 20;
+let oranges = 30;
+console.log("apples = " + apples, "oranges = " + oranges); // apples = 20 oranges = 30
+
+// 1.
+console.log(apples == oranges); // false
+
+// 2.
+console.log(apples != oranges); // true
+
+// 3.
+console.log(apples > oranges); // false
+
+// 4.
+console.log(apples <= oranges); // true
+
+// 5.
+console.log(apples < oranges); // true
+
+// 6.
+let mangoes = 5;
+console.log(mangoes * apples > mangoes + oranges); // true
+
+// 7.
+console.log(apples - mangoes < oranges / mangoes); // false
+
+// 8.
+console.log(mangoes == apples == oranges); // false
+
+// 9.
+console.log(apples % mangoes) == (oranges % mangoes); // 0
+
+// 10.
+console.log(mangoes + apples > oranges - mangoes); // false
+console.log(mangoes++ + apples > oranges - mangoes); // true
+
